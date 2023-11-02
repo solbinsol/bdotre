17a009ca4e27e9c0d5bb36b3a1e556451a9c80eb
solbinsol <qkaejwnj@naver.com>
2023-11-02 11:16:39 +0900

    test

---
+++ src/component/Slider/Slider.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import Link from "next/link";
+import style from './Slider.module.css';
+export default class Resizable extends Component {
+  state = {
+    display: true,
+    width: 100,
+    height:600,
+
+  };
+  render() {
+    const settings = {
+
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows:true,
+    };
+    return (
+      <div className="Slid">
+
+
+        <div
+          style={{
+            width: this.state.width + "%",
+            height: this.state.height + "px",
+            display: this.state.display ? "block" : "none"
+          }}
+        >
+          <Slider {...settings}>
+            <div className="SlidItem">
+                <Link href="detail1"><img src="./images/1.jpg" alt="ss" /></Link>
+            </div  >
+            <div className="SlidItem">
+            <Link href="detail2"><img src="./images/2.jpg" alt="ss" /></Link>
+            </div>
+            <div className="SlidItem">
+            <Link href="detail3"><img src="./images/3.jpg" alt="ss" /></Link>
+            </div>
+            <div className="SlidItem">
+            <Link href="detail4"><img src="./images/4.jpg" alt="ss" /></Link>
+            </div>
+
+          </Slider>
+        </div>
+        <div className="Footer">
+            
+        </div>
+      </div>
+    );
+  }
+}
